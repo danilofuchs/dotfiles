d830272a30bfa0a1fdaf1d7e9745f9e029529618
Danilo Campana Fuchs <danilo.fuchs@ebanx.com>
2021-05-18 12:49:27 -0300

    kill_ports script

--- install.sh
+++ install.sh
@@ -13,6 +13,9 @@ ln -s `pwd`/.hyper.js ~/.hyper.js
 mv ~/.starship.toml ~/.starship.toml.backup
 ln -s `pwd`/.starship.toml ~/.starship.toml
 
+chmod +x `pwd`/kill_ports
+ln -s `pwd`/kill_ports /usr/local/bin/kill_ports
+
 git clone https://github.com/danilofuchs/pug ~/pug
 
 . ~/.zshrc
